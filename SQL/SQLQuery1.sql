8b31283843086c5cbade15f1dcb5c4c9a3ee5748
Willian Cuartas <william.cuartas@peaku.io>
2023-07-14 17:45:23 -0500

    base de datos inicial
    sql de creacion de las tablas y de los procedimientos

---
+++ SQL/SQLQuery1.sql
@@ -0,0 +1,42 @@
+use PruebaBig_WillianFerneyCuartasMesa
+
+create table users(
+	Id int IDENTITY(1,1) PRIMARY KEY,
+	Email varchar(50) not null unique,
+	Password varchar(50) not null,
+	FullName varchar(50) not null,
+	User_Admin bit not null 
+)
+
+create table cities(
+	Id int IDENTITY(1,1) PRIMARY KEY,
+	Name varchar(50),
+	Departament varchar(50)
+)
+
+create table forecasts(
+	Id int IDENTITY(1,1) PRIMARY KEY,
+	Title varchar(50),
+	DateClima date not null,
+	MinTemperature int not null,
+	MaxTemperature int not null,
+	RainProbability float not null,
+	Observation varchar(200),
+	IdCity int,
+	IdUserChage int not null,
+	DateChange datetime not null
+)
+
+
+ALTER TABLE forecasts
+ADD FOREIGN KEY (IdCity) REFERENCES cities(Id);
+
+
+ALTER TABLE forecasts
+ADD CONSTRAINT UniqueCitiDate UNIQUE (DateClima,IdCity);
+
+
+insert into cities values('Bogotá','Cundinamarca'),('Medellin','Antioquia'),('Cali','Valle del cauca'),('Pasto','Nariño')
+
+select * from cities
+
---
+++ SQL/procedimientos.sql
@@ -0,0 +1,124 @@
+CREATE OR ALTER PROCEDURE SP_PostForecast
+    @Title VARCHAR(50),
+    @DateClima DATE,
+    @MinTemperature INT,
+    @MaxTemperature INT,
+    @RainProbability FLOAT,
+    @Observation VARCHAR(200),
+    @IdCity INT,
+    @IdUserChange INT
+AS
+BEGIN
+    INSERT INTO forecasts (Title, DateClima, MinTemperature, MaxTemperature, RainProbability, Observation, IdCity, IdUserChage, DateChange)
+    VALUES (@Title, @DateClima, @MinTemperature, @MaxTemperature, @RainProbability, @Observation, @IdCity, @IdUserChange, GETDATE())
+END
+
+
+
+EXEC SP_PostForecast
+    @Title = 'Título del pronóstico citi2',
+    @DateClima = '2023-07-20',
+    @MinTemperature = 30,
+    @MaxTemperature = 40,
+    @RainProbability = 0.2,
+    @Observation = 'Observaciones del clima',
+    @IdCity = 1,
+    @IdUserChange = 123
+
+
+
+SELECT * FROM forecasts
+
+///////* procedimiento editar clima */
+
+CREATE OR ALTER PROCEDURE SP_PutForecast
+	@Id int,
+    @Title VARCHAR(50),
+    @DateClima DATE,
+    @MinTemperature INT,
+    @MaxTemperature INT,
+    @RainProbability FLOAT,
+    @Observation VARCHAR(200),
+    @IdCity INT,
+    @IdUserChange INT
+AS
+BEGIN
+    UPDATE forecasts
+    SET Title = @Title,
+        DateClima = @DateClima,
+        MinTemperature = @MinTemperature,
+        MaxTemperature = @MaxTemperature,
+        RainProbability = @RainProbability,
+        Observation = @Observation,
+        IdCity = @IdCity,
+        IdUserChage = @IdUserChange,
+        DateChange = GETDATE()
+    WHERE Id = @Id
+END
+
+
+EXEC SP_PutForecast
+    @id=1,
+    @Title = 'Título del pronóstico edit',
+    @DateClima = '2023-07-16',
+    @MinTemperature = 20,
+    @MaxTemperature = 30,
+    @RainProbability = 0.3,
+    @Observation = 'Observaciones del clima',
+    @IdCity = 2,
+    @IdUserChange = 122
+
+SELECT * FROM forecasts
+
+/*CONSULTAR POR ID */
+CREATE OR ALTER PROCEDURE SP_GetForecast
+    @Id int
+AS
+BEGIN
+    SELECT Id, Title, DateClima, MinTemperature, MaxTemperature, RainProbability, Observation, IdCity
+    FROM  forecasts WHERE Id = @Id
+END
+
+EXEC SP_GetForecast
+    @Id=1
+
+/*CONSULTAR POR FECHA */
+CREATE OR ALTER PROCEDURE SP_ListDateForecast
+    @DateClima DATE
+AS
+BEGIN
+    SELECT Id, Title, DateClima, MinTemperature, MaxTemperature, RainProbability, Observation, IdCity
+    FROM  forecasts WHERE DateClima = @DateClima
+END
+
+EXEC SP_ListDateForecast
+    @DateClima = '2023-07-16'
+
+	
+/*CONSULTAR POR Ciudad */
+CREATE OR ALTER PROCEDURE SP_ListCityForecast
+    @IdCity int
+AS
+BEGIN
+    SELECT TOP 5 Id, Title, DateClima, MinTemperature, MaxTemperature, RainProbability, Observation, IdCity 
+    FROM  forecasts WHERE IdCity = @IdCity AND DateClima>= CAST(GETDATE() AS DATE) ORDER BY DateClima 
+END
+
+EXEC SP_ListCityForecast
+    @IdCity = 1
+
+
+
+/*CONSULTAR POR Ciudad y fecha */
+CREATE OR ALTER PROCEDURE SP_ListCityDateForecast
+    @IdCity int,
+	@DateClima DATE
+AS
+BEGIN
+    SELECT Title, DateClima, MinTemperature, MaxTemperature, RainProbability, Observation, IdCity 
+    FROM  forecasts WHERE IdCity = @IdCity AND DateClima = @DateClima
+END
+
+EXEC SP_ListCityDateForecast
+    @IdCity = 1,
+	@DateClima = '2023-07-17'
